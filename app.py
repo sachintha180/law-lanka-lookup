cff1ffcccc731c048de89692e8dca07726a1e4b9
sachintha180 <sachinthasenanayake180@gmail.com>
2024-12-29 10:57:41 +0530

    add Jinja for-loop for populating feature list in landing.html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from data import features
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def landing():
-    return render_template("landing.html")
+    return render_template("landing.html", features=features)
 
 
 if __name__ == "__main__":
---
+++ data.py
@@ -0,0 +1,14 @@
+features = [
+    {
+        "title": "Manage Documents",
+        "description": "Upload, organize, and share your legal documents with ease.",
+    },
+    {
+        "title": "Connect with Lawyers",
+        "description": "Build your network and collaborate with others in the legal field.",
+    },
+    {
+        "title": "AI-Powered Chat",
+        "description": "Interact with your documents using an AI chatbot for efficient querying.",
+    },
+]
