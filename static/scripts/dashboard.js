a45ee261c3872448a7c603d3ad624df3af615739
sachintha180 <sachinthasenanayake180@gmail.com>
2024-12-29 15:05:29 +0530

    updated badge colors

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -2,8 +2,13 @@ window.onload = init;
 
 function init() {
   const logoImg = document.getElementById("logo");
+  const logoutBtn = document.getElementById("logout");
 
   logoImg.onclick = function () {
+    window.location.href = "/";
+  };
+
+  logoutBtn.onclick = function () {
     window.location.href = "/";
   };
 }
