d5934b52b89b61b7853d68bc5b4b045e0ee2f7d2
sachintha180 <sachinthasenanayake180@gmail.com>
2024-12-29 14:58:34 +0530

    add dashboard and associated mock data with accompanying routes

--- app.py
+++ app.py
@@ -19,5 +19,62 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template(
+        "dashboard.html",
+        user={
+            "first_name": "Sachintha",
+            "name": "Sachintha Senanayake",
+            "email": "sachintha@gmail.com",
+            "expertise": "Conveyancer",
+        },
+        latest_reads=[
+            {
+                "name": "Document 1",
+                "type": "pdf",
+                "date": "2021-01-01",
+            },
+            {
+                "name": "Document 2",
+                "type": "pdf",
+                "date": "2021-01-02",
+            },
+            {
+                "name": "Document 3",
+                "type": "docx",
+                "date": "2021-01-03",
+            },
+            {
+                "name": "Document 4",
+                "type": "docx",
+                "date": "2021-01-04",
+            },
+        ],
+        last_online=[
+            {
+                "name": "Alice",
+                "expertise": "Lawyer",
+                "last_online": "2021-01-01",
+            },
+            {
+                "name": "Bob",
+                "expertise": "Litigator",
+                "last_online": "2021-01-02",
+            },
+            {
+                "name": "Charlie",
+                "expertise": "Conveyancer",
+                "last_online": "2021-01-03",
+            },
+            {
+                "name": "David",
+                "expertise": "Lawyer",
+                "last_online": "2021-01-04",
+            },
+        ],
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -2,13 +2,8 @@ window.onload = init;
 
 function init() {
   const logoImg = document.getElementById("logo");
-  const loginBtn = document.getElementById("login");
 
   logoImg.onclick = function () {
     window.location.href = "/";
-  };
-
-  loginBtn.onclick = function () {
-    window.location.href = "/login";
   };
 }
